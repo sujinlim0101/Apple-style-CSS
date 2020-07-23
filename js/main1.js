39f7cf71a69f974a99d880bfdf201b552fd0ec9b
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-07-23 21:29:28 +0900

    js initial 화면에 따른 height 조정

---
+++ js/main1.js
@@ -0,0 +1,49 @@
+(() => {
+    let sceneInfo = [
+        {//0
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-0')
+            }
+
+        },
+        {//1
+            type: 'normal',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1')
+            }
+
+        },
+        {//2
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2')
+            }
+
+        },
+        {//3
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3')
+            }
+
+        },
+    ];
+    function setLayout() {
+        for (let i = 0; i < sceneInfo.length; i++) {
+                sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight,
+                sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        };
+        console.log(sceneInfo);
+    }
+    window.addEventListener('resize', setLayout);
+    setLayout();
+})()
