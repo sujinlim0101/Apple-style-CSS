ffe644384a737a5bc054f6e42fd80881c8234ed5
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-07-29 15:06:15 +0900

    비디오 인터렉션 추가

--- js/main1.js
+++ js/main1.js
@@ -16,9 +16,15 @@
                 messageA: document.querySelector('#scroll-section-0 .main-message.a'),
                 messageB: document.querySelector('#scroll-section-0 .main-message.b'),
                 messageC: document.querySelector('#scroll-section-0 .main-message.c'),
-                messageD: document.querySelector('#scroll-section-0 .main-message.d')
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+                canvas: document.querySelector("#video-canvas-0"),
+                context: document.querySelector("#video-canvas-0").getContext('2d'),
+                videoImages: []
             },
             values: {
+                videomImageCount: 300,
+                imgaeSequence: [0, 299],
+                canvas_opacity: [1,0, { start: 0.9, end: 1}],
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
                 messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
                 messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -57,9 +63,16 @@
                 messageB: document.querySelector('#scroll-section-2 .b'),
                 messageC: document.querySelector('#scroll-section-2 .c'),
                 pinB: document.querySelector('#scroll-section-2 .b .pin'),
-                pinC: document.querySelector('#scroll-section-2 .c .pin')
+                pinC: document.querySelector('#scroll-section-2 .c .pin'),
+                canvas: document.querySelector("#video-canvas-1"),
+                context: document.querySelector("#video-canvas-1").getContext('2d'),
+                videoImages: []
             },
             values: {
+                videomImageCount: 960,
+                imgaeSequence: [0, 959],
+                canvas_opacity_in: [0,1, { start: 0, end: 0.1}],
+                canvas_opacity_out: [1,0, { start: 0.95, end: 1}],
                 messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
                 messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
                 messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
@@ -91,6 +104,25 @@
         }
     ];
     
+    function setCanvasImages() {
+        let imgElem;
+        for (let i = 0; i < sceneInfo[0].values.videomImageCount; i++) {
+            imgElem = new Image();
+            imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+            sceneInfo[0].objs.videoImages.push(imgElem);
+        }
+        console.log(sceneInfo[0].objs.videoImages);
+        let imgElem2;
+        for (let i = 0; i < sceneInfo[2].values.videomImageCount; i++) {
+            imgElem2 = new Image();
+            imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
+            sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
+        console.log(sceneInfo[2].objs.videoImages);
+
+    }
+    setCanvasImages();
+
     function setLayout() {
         for (let i = 0; i < sceneInfo.length; i++) {
             if (sceneInfo[i].type === 'sticky') {
@@ -111,6 +143,9 @@
             }
         }
         document.body.setAttribute('id',`show-scene-${currentScene}`);
+        const heightRatio = window.innerHeight/1080;
+        sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+        sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 
     }
 	function calcValues(values, currentYOffset) {
@@ -148,6 +183,9 @@
     
         switch (currentScene) {
             case 0:
+                let sequence = Math.round(calcValues(values.imgaeSequence, currentYOffset));
+                objs.context.drawImage(objs.videoImages[sequence],0,0);
+                objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
                 // console.log('0 play');
                 if (scrollRatio <= 0.22) {
                     // in
@@ -193,6 +231,16 @@
     
             case 2:
                 // console.log('2 play');
+                let sequence2 = Math.round(calcValues(values.imgaeSequence, currentYOffset));
+                objs.context.drawImage(objs.videoImages[sequence2],0,0);
+                
+                if (scrollRatio <= 0.5) {
+                    objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset);
+                } else {
+                    objs.canvas.style.opacity = calcValues(values.canvas_opacity_out, currentYOffset);
+
+                }
+                
                 if (scrollRatio <= 0.32) {
                     // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -262,7 +310,11 @@
         scrollLoop();
     })
     //load 대신 DOMContentLoaded 써도됨
-    window.addEventListener('load', setLayout);
+    window.addEventListener('load', () => {
+        setLayout();
+        sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0],0,0);
+
+    });
     window.addEventListener('resize', setLayout);
 
 })(); 
