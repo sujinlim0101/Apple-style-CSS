8011cff828c5979c2e1acc264acf59efc535255d
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-07-31 22:01:56 +0900

    블랜딩 애니메이션 추가

--- js/main1.js
+++ js/main1.js
@@ -107,7 +107,11 @@
             values: {
                 rect1X: [ 0, 0, { start: 0, end: 0 }],
                 rect2X: [ 0, 0, { start: 0, end: 0 }],
-                rectStartY: 0
+                imageBlendY: [ 0, 0, { start: 0, end: 0 }],
+                blendHeight: [ 0, 0, { start: 0, end: 0 } ],
+                rectStartY: 0,
+                canvas_scale: [ 0, 0, { start: 0, end: 0 } ],
+
             }
         }
     ];
@@ -206,7 +210,6 @@
                 } else {
                     // out
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
-                    console.log(objs.messageA.style.opacity);
                     objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`;
                 }
     
@@ -304,7 +307,7 @@
                     }
                     objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
                     objs.context.fillStyle = 'white';
-                        objs.context.drawImage(objs.images[0],0,0);
+                    objs.context.drawImage(objs.images[0],0,0);
 
                     //캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
                     const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
@@ -377,16 +380,43 @@
 					parseInt(whiteRectWidth),
 					objs.canvas.height
                 );
-                //캔버스가 브라우저 상단에 닿지 않았다면
+                //캔버스가 브라우저 상단에 닿는 조건으로 분기
                 if (scrollRatio < values.rect1X[2].end) {
                     step = 1;
                     objs.canvas.classList.remove('sticky');
                     // console.log('캔버스가 인닿았다.')
                 } else {
                     step = 2;
+                    values.blendHeight[0] = 0;
+					values.blendHeight[1] = objs.canvas.height;
+					values.blendHeight[2].start = values.rect1X[2].end;
+					values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+					const blendHeight = calcValues(values.blendHeight, currentYOffset);
+            
+					objs.context.drawImage(objs.images[1],
+						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+					);
+
                     // console.log('캔버스가 닿았다!')
                     objs.canvas.classList.add('sticky');
                     objs.canvas.style.top = `-${(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
+
+                    if (scrollRatio > values.blendHeight[2].end) {
+                        values.canvas_scale[0] = canvasScaleRatio;
+                        values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width); 
+                        values.canvas_scale[2].start = values.blendHeight[2].end;
+                        values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
+
+                        objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`;
+                        objs.canvas.style.marginTop = 0;
+                    }
+
+                    if (scrollRatio > values.canvas_scale[2].end && values.canvas_scale[2].end > 0) {
+                        objs.canvas.classList.remove('sticky');
+                        objs.canvas.style.marginTop = `${scrollHeight*0.4}px`;
+
+                    }
                 }
                 break;
         }
